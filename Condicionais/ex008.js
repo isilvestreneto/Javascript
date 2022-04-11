db2d7d73d7d29dd9f5fa1cf388065dbe061c74bb
Ivan Silvestre <isilvestreneto@gmail.com>
2022-04-11 08:12:50 -0300

    pratica11

---
+++ Condicionais/ex008.js
@@ -0,0 +1,6 @@
+var vel = 72.5
+console.log(`A velocidade do seu carro é ${vel}Km/H.`)
+if (vel>60) { // Condicional simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre utilizando o cinto de segurança.`)
---
+++ Condicionais/ex009.js
@@ -0,0 +1,6 @@
+var país = 'BRA'
+if (país == 'BRA') {
+    console.log('Você é brasileiro.')
+} else {
+    console.log('Você é estrangeiro.')
+}
